c9c4cbd174700b986f3f30cbb042cc004ec9d84c
Tolga Durukan <tolgadurukan16@gmail.com>
2023-02-26 22:23:16 +0200

    integrated be

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -14,11 +14,10 @@ export default function Products() {
   if (isLoading) {
     return <Typography>Loading...</Typography>;
   }
-
   return (
     <Grid container spacing={4}>
       {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-      {data.map(({ count, ...product }, index) => (
+      {data?.map(({ count, ...product }, index) => (
         <Grid item key={product.id} xs={12} sm={6} md={4}>
           <Card
             sx={{ height: "100%", display: "flex", flexDirection: "column" }}
